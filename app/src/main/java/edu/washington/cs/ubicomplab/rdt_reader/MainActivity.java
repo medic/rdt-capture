21d4e6a42652b8232aae13048e1746cc645a5622
Chunjong Park <cjparkuw@cs.washington.edu>
2018-01-14 15:50:05 -0800

    Initial commit

---
+++ app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/MainActivity.java
@@ -0,0 +1,159 @@
+package edu.washington.cs.ubicomplab.rdt_reader;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2{
+    private static final String TAG = "rdt-reader:MainActivity";
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private ConstraintLayout mContainer;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_CAMERA_REQUEST_CODE);
+        }
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Log.i(TAG, "called onCreate");
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mContainer = findViewById(R.id.container);
+
+        mOpenCvCameraView = findViewById(R.id.opencv_camera_view);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        //mOpenCvCameraView.setMaxFrameSize(container.getMaxWidth(), container.getMinHeight());
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        /*Mat newFrame = new Mat();
+
+        double hRatio;
+        double wRatio;
+
+        hRatio = mContainer.getHeight()/inputFrame.rgba().height();
+        wRatio = mContainer.getWidth()/inputFrame.rgba().width();
+
+        Log.d(TAG, String.format("%d,%d %d.%d", mContainer.getHeight(), mContainer.getWidth(), inputFrame.rgba().height(), inputFrame.rgba().width()));
+
+        if (hRatio < wRatio)
+            Imgproc.resize(inputFrame.rgba(), newFrame, new Size(inputFrame.rgba().width()*hRatio, inputFrame.rgba().height()*hRatio));
+        else
+            Imgproc.resize(inputFrame.rgba(), newFrame, new Size(inputFrame.rgba().width()*wRatio, inputFrame.rgba().height()*wRatio));
+
+        return newFrame;*/
+
+
+
+        /*Mat mRgba = inputFrame.rgba();
+        Mat mRgbaT = mRgba.t();
+        Core.flip(mRgba.t(), mRgbaT, 1);
+        Imgproc.resize(mRgbaT, mRgbaT, new Size(mContainer.getWidth(), mContainer.getHeight()));
+
+        Log.d(TAG, String.format("%d,%d %d,%d %d,%d", mContainer.getHeight(), mContainer.getWidth(), mOpenCvCameraView.getHeight(), mOpenCvCameraView.getWidth(), inputFrame.rgba().height(), inputFrame.rgba().width()));
+
+
+        return mRgbaT;*/
+
+        Log.d(TAG, String.format("%d,%d %d.%d", mContainer.getHeight(), mContainer.getWidth(), inputFrame.rgba().height(), inputFrame.rgba().width()));
+
+        return inputFrame.rgba();
+    }
+}
