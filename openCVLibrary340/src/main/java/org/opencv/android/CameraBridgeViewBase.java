949a6ebe826294b31a43780cbe4826d51cf42cba
Chunjong Park <cjparkuw@cs.washington.edu>
2018-03-06 18:37:45 -0800

    Change opencv version to support Feature2D

--- openCVLibrary340/src/main/java/org/opencv/android/CameraBridgeViewBase.java
+++ openCVLibrary340/src/main/java/org/opencv/android/CameraBridgeViewBase.java
@@ -1,5 +1,12 @@
 package org.opencv.android;
 
+import java.util.List;
+
+import org.opencv.BuildConfig;
+import org.opencv.R;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -13,12 +20,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
-import org.opencv.R;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
-
-import java.util.List;
 
 /**
  * This is a basic class, implementing the interaction with Camera and OpenCV library.
