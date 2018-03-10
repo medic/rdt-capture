5125e9cdd5e53c151da179d5d3d7047ba7721523
Chunjong Park <cjparkuw@cs.washington.edu>
2018-03-10 15:37:20 -0800

    Implement an image quality checker with a simple contour detection

--- app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/ImageQualityActivity.java
+++ app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/ImageQualityActivity.java
@@ -3,16 +3,93 @@ package edu.washington.cs.ubicomplab.rdt_reader;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class ImageQualityActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static edu.washington.cs.ubicomplab.rdt_reader.Constants.TAG;
+
+public class ImageQualityActivity extends AppCompatActivity implements CvCameraViewListener2{
+
+    private RDTCameraView mOpenCvCameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    /*Activity callbacks*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_quality);
 
         setTitle("Image Quality Checker");
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mOpenCvCameraView = findViewById(R.id.img_quality_check_camera_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    /*Activity callbacks*/
+    @Override
+    protected void onPause() {
+        super.onPause();
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 
     @Override
@@ -26,5 +103,73 @@ public class ImageQualityActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    /*OpenCV JavaCameraView callbacks*/
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        return drawContourUsingSobel(inputFrame.rgba());
+    }
+
+    /*Private methods*/
+    private Mat drawContourUsingSobel(Mat input) {
+        Mat sobelx = new Mat();
+        Mat sobely = new Mat();
+        Mat output = new Mat();
+        Mat sharp = new Mat();
+
+        //Imgproc.GaussianBlur(input, output, new Size(21, 21), 8);
+        Imgproc.GaussianBlur(input, output, new Size(21, 21), 3);
+        Imgproc.cvtColor(output, output, Imgproc.COLOR_RGB2GRAY);
+
+        Imgproc.Sobel(output, sobelx, CvType.CV_32F, 0, 1); //ksize=5
+        Imgproc.Sobel(output, sobely, CvType.CV_32F, 1, 0); //ksize=5
+
+        Core.pow(sobelx, 2, sobelx);
+        Core.pow(sobely, 2, sobely);
+
+        Core.add(sobelx, sobely, output);
+
+        output.convertTo(output, CvType.CV_32F);
+
+        Core.pow(output, 0.5, output);
+        Core.multiply(output, new Scalar(Math.pow(2, 0.5)),output);
+
+        output.convertTo(output, CvType.CV_8UC1);
+
+        Imgproc.GaussianBlur(output, sharp, new Size(0, 0), 3);
+        Core.addWeighted(output, 1.5, sharp, -0.5, 0, output);
+        Core.bitwise_not(output, output);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+
+        Imgproc.findContours(output, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        Log.d(TAG, "contours: " + contours.size());
+
+        //output.convertTo(output, CV_32F);
+
+        //for(int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
+        for( int idx = 0; idx < contours.size(); idx++ ) {
+            MatOfPoint matOfPoint = contours.get(idx);
+            Rect rect = Imgproc.boundingRect(matOfPoint);
+            if(rect.size().width > 100 && rect.size().height > 100)
+                Imgproc.rectangle(input, rect.tl(), rect.br(), new Scalar(255, 255, 255));
+        }
+
+        return input;
+
     }
 }
