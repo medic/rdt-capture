5ab12ee436f338501649235a47f64e92afca867b
Alex Anderson <anderson@medicmobile.org>
2018-06-29 10:04:50 +0000

    Make "Constants" constant

--- app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/Constants.java
+++ app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/Constants.java
@@ -14,27 +14,26 @@ public final class Constants {
     public static final int MY_PERMISSION_REQUEST_CODE = 100;
     public static final String[] DATE_FORMATS = {"yyyy/MM/dd","yyyy.MM.dd","yyyy-MM-dd", "yyyyMMdd"};
 
-    public static double BLUR_THRESHOLD = 0.0;
-    public static double OVER_EXP_THRESHOLD = 255;
-    public static double UNDER_EXP_THRESHOLD = 120;
-    public static double OVER_EXP_WHITE_COUNT = 50;
+    public static final double BLUR_THRESHOLD = 0.0;
+    public static final double OVER_EXP_THRESHOLD = 255;
+    public static final double UNDER_EXP_THRESHOLD = 120;
+    public static final double OVER_EXP_WHITE_COUNT = 50;
 
-    public static String OK = "<font color='#00EE00'>✔</font>";
-    public static String NOT_OK = "<font color='#EE0000'>✘</font>";
+    public static final String OK = "<font color='#00EE00'>✔</font>";
+    public static final String NOT_OK = "<font color='#EE0000'>✘</font>";
 
-    public static Scalar RDT_COLOR_HSV = new Scalar(30, 21, 204, 0.0);
+    public static final Scalar RDT_COLOR_HSV = new Scalar(30, 21, 204, 0.0);
 
-    public static double SIZE_THRESHOLD = 0.2;
-    public static double POSITION_THRESHOLD = 0.2;
+    public static final double SIZE_THRESHOLD = 0.2;
+    public static final double POSITION_THRESHOLD = 0.2;
 
-    public static int CALIBRATION_FRAME_COUNTER = 1;
-    public static int FEATURE_MATCHING_FRAME_COUNTER = 0;
-    public static int CAPTURE_COUNT = 3;
+    public static final int CALIBRATION_FRAME_COUNTER = 1;
+    public static final int FEATURE_MATCHING_FRAME_COUNTER = 0;
+    public static final int CAPTURE_COUNT = 3;
 
-    public static Size PREVIEW_SIZE = new Size(960, 720);
-    public static double VIEWPORT_SCALE = 0.50;
+    public static final double VIEWPORT_SCALE = 0.50;
 
-    public static String LANGUAGE = "en";
+    public static final String LANGUAGE = "en";
 
-    public static String RDT_IMAGE_DIR = Environment.getExternalStorageDirectory() + "/Pictures/" +"/RDTImageCaptures/";
+    public static final String RDT_IMAGE_DIR = Environment.getExternalStorageDirectory() + "/Pictures/" +"/RDTImageCaptures/";
 }
