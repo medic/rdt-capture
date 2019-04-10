4252596e27f08d8bd587269722b24706dfa0958c
Chunjong Park <cjparkuw@cs.washington.edu>
2019-04-09 19:29:18 -0700

    Locate result window

--- app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/Constants.java
+++ app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/Constants.java
@@ -17,7 +17,7 @@ public final class Constants {
     public static double BLUR_THRESHOLD = 0.0;
     public static double OVER_EXP_THRESHOLD = 255;
     public static double UNDER_EXP_THRESHOLD = 120;
-    public static double OVER_EXP_WHITE_COUNT = 100;
+    public static double OVER_EXP_WHITE_COUNT = 200;
 
     public static String OK = "<font color='#00EE00'>✔</font>";
     public static String NOT_OK = "<font color='#EE0000'>✘</font>";
@@ -29,7 +29,7 @@ public final class Constants {
 
     public static int CALIBRATION_FRAME_COUNTER = 1;
     public static int FEATURE_MATCHING_FRAME_COUNTER = 0;
-    public static int CAPTURE_COUNT = 3;
+    public static int CAPTURE_COUNT = 1;
 
     public static Size PREVIEW_SIZE = new Size(960, 720);
     public static Size CAMERA2_PREVIEW_SIZE = new Size(1280, 720);
