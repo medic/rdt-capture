2c16102dcb22d45933eb2ea1295738ce58bef22e
Ram Kandasamy <ram@auderenow.org>
2019-06-07 14:44:03 -0700

    Add back a simple ImageQualityActivity to wrap ImageQualityView

---
+++ app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/ImageQualityActivity.java
@@ -0,0 +1,12 @@
+package edu.washington.cs.ubicomplab.rdt_reader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageQualityActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_quality);
+    }
+}
