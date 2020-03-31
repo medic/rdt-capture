2f22d6b83b8fd5f171acf70fe0e56ca3c9082854
atm15 <atm15@cs.washington.edu>
2020-03-30 21:54:11 -0700

    intermediate push for RDTCameraView

--- app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/views/RDTCameraView.java
+++ app/src/main/java/edu/washington/cs/ubicomplab/rdt_reader/views/RDTCameraView.java
@@ -51,59 +51,77 @@ public class RDTCameraView extends JavaCameraView implements Camera.AutoFocusCal
 
     /**
      * Focuses the camera wherever the user touches on the screen
-     * Note: we do nto use this function since we handle auto-focus ourselves
+     * Note: we do not use this function since we handle auto-focus ourselves
      * @param event: the touch event
      */
     public void focusOnTouch(MotionEvent event) {
+        // Identify a small rectangle where the frame should be focused
         Rect focusRect = calculateTapArea(event.getRawX(), event.getRawY(), 1f);
+
+        // Identify a slightly larger rectangle to use as a reference
         Rect meteringRect = calculateTapArea(event.getRawX(), event.getRawY(), 1.5f);
 
+        // Prepare the camera's parameters
         Camera.Parameters parameters = mCamera.getParameters();
         parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
 
+        // Update focus parameters
         if (parameters.getMaxNumFocusAreas() > 0) {
             List<Camera.Area> focusAreas = new ArrayList<Camera.Area>();
             focusAreas.add(new Camera.Area(focusRect, 1000));
-
             parameters.setFocusAreas(focusAreas);
         }
 
+        // Update metering parameters
         if (parameters.getMaxNumMeteringAreas() > 0) {
             List<Camera.Area> meteringAreas = new ArrayList<Camera.Area>();
             meteringAreas.add(new Camera.Area(meteringRect, 1000));
-
             parameters.setMeteringAreas(meteringAreas);
         }
 
+        // Apply new camera parameters
         mCamera.setParameters(parameters);
         mCamera.autoFocus(this);
     }
 
     /**
-     * Convert touch position x:y to {@link Camera.Area} position -1000:-1000 to 1000:1000.
+     * Convert a touch position to a rectangle that is scaled
+     * between (-focusDim, -focusDim) to (focusDim, focusDim)
+     * @param x: the touch position's x-coordinate
+     * @param y: the touch position's y-coordinate
+     * @param coefficient: the scale of the rectangle relative to a default parameter
+     * @return a {@link Rect} indicating where the user touched within a region of fixed scale
      */
     private Rect calculateTapArea(float x, float y, float coefficient) {
-        float focusAreaSize = 300;
-        int areaSize = Float.valueOf(focusAreaSize * coefficient).intValue();
+        // Get the scale of the rectangle
+        int focusDim = 1000;
+        double focusFraction = 0.3;
+        double focusAreaSize = focusFraction*focusDim;
+        int areaSize = Double.valueOf(focusAreaSize * coefficient).intValue();
 
-        int centerX = (int) (x / getResolution().width - 1000);
-        int centerY = (int) (y / getResolution().height - 1000);
+        // Compute the dimensions of the rectangle
+        int centerX = (int) (x / getResolution().width - focusDim);
+        int centerY = (int) (y / getResolution().height - focusDim);
+        int left = clamp(centerX - areaSize / 2, -focusDim, focusDim);
+        int top = clamp(centerY - areaSize / 2, -focusDim, focusDim);
 
-        int left = clamp(centerX - areaSize / 2, -1000, 1000);
-        int top = clamp(centerY - areaSize / 2, -1000, 1000);
-
+        // Create the rectangle
         RectF rectF = new RectF(left, top, left + areaSize, top + areaSize);
-
         return new Rect(Math.round(rectF.left), Math.round(rectF.top), Math.round(rectF.right), Math.round(rectF.bottom));
     }
 
+    /**
+     *
+     * @param x
+     * @param min
+     * @param max
+     * @return
+     */
     private int clamp(int x, int min, int max) {
-        if (x > max) {
+        if (x > max)
             return max;
-        }
-        if (x <min) {
+        if (x < min)
             return min;
-        }
         return x;
     }
 
